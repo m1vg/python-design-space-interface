87fec7edfb498f665f6dbd1a8bb2bd11f2c17daf
Jason Lomnitz <jlomn@ucdavis.edu>
2014-09-18 14:44:25 -0700

    Added a version tag to the dspace package

--- dspace/__init__.py
+++ dspace/__init__.py
@@ -5,3 +5,5 @@ from dspace.models.designspace import DesignSpace
 from dspace.models.case import Case, CaseIntersection
 from dspace.variables import VariablePool
 from dspace.expressions import Expression
+
+__version__ = '0.2.0-dev'
