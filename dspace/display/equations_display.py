d39cc0b84e1069e6541c02102d67e03dfd5eec03
Jason Lomnitz <jlomn@ucdavis.edu>
2014-09-24 16:14:21 -0700

    Modified the way equations are represented using the IPython rich display system

--- dspace/display/equations_display.py
+++ dspace/display/equations_display.py
@@ -8,8 +8,10 @@ from IPython.display import display, Math
 
 @monkeypatch_method(dspace.models.base.Equations)
 def __repr__(self):
+    string = r'\begin{array}{rcl}'
     for i in self.system:
         eq = Expression(i)
-        string = eq.__latex_str__(substitution_dictionary=self._latex)
-        display(Math(string))
-    return ''
+        string += eq.__latex_str__(substitution_dictionary=self._latex)
+        string += r'\\'
+    string += r'\end{array}'
+    return Math(string)
