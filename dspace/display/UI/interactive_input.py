d73a832c8140d8b0eadade5232a944aa3f6eb87d
System Administrator <jlomn@ucdavis.edu>
2015-08-15 22:22:26 -0700

    Modified keyword argument for kinetic order architectural constraints.

--- dspace/display/UI/interactive_input.py
+++ dspace/display/UI/interactive_input.py
@@ -361,7 +361,7 @@ class InteractiveInput(object):
         self.name = b.name.value
         constraints = self.constraints
         replacements = str(b.replacements.value).strip()
-        self.options.update(kinetic_orders=replacements)
+        self.options.update(kinetic_orders=b.replacements.value)
         if len(replacements) > 0:
             replacements = str(b.replacements.value).split(',')
             replacements = [[j.strip() for j in i.split('=')] for i in replacements] 
