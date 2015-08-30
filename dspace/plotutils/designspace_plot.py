c7de1f91a615560ce397115b0e951c80b9953621
Jason Lomnitz <jlomn@ucdavis.edu>
2015-08-30 12:59:01 -0700

    Modified the 3D design space plot included code section.

--- dspace/plotutils/designspace_plot.py
+++ dspace/plotutils/designspace_plot.py
@@ -515,9 +515,10 @@ def draw_3D_slice(self, ax, p_vals, x_variable, y_variable,z_variable, range_x,
         valid_nonstrict = [i for i in valid_nonstrict if i not in valid_cases]
     print 2
     print valid_cases
-    case_int_list = self.intersecting_cases(intersections, 
-                                            valid_cases,
-                                            p_bounds=p_bounds)
+    ## case_int_list = self.intersecting_cases(intersections, 
+    ##                                         valid_cases,
+    ##                                         p_bounds=p_bounds)
+    case_int_list = valid_cases
     print 3
     if color_dict is None:
         color_dict = dict()
