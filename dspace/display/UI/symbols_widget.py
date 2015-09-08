308ccecac6ca4cf69c47e51d7cd98b78c6f06b41
System Administrator <jlomn@ucdavis.edu>
2015-09-08 15:15:55 -0700

    Updated symbols widget for IPython 4.0.0

--- dspace/display/UI/symbols_widget.py
+++ dspace/display/UI/symbols_widget.py
@@ -2,9 +2,31 @@ import dspace
 import dspace.plotutils
 import dspace.display
 
-from IPython.html.widgets import interact, interactive, fixed
-from IPython.html import widgets
-from IPython.display import clear_output, display, HTML, Latex
+from distutils.version import LooseVersion, StrictVersion
+
+import IPython
+
+if StrictVersion(IPython.__version__) < StrictVersion('4.0.0'):
+    from IPython.html.widgets import interact, interactive, fixed
+    from IPython.html.widgets import HTMLWidget as HTML
+    from IPython.html.widgets import TabWidget as Tab
+    from IPython.html.widgets import CheckboxWidget as Checkbox
+    from IPython.html.widgets import ButtonWidget as Button
+    from IPython.html.widgets import ContainerWidget as Box
+    from IPython.html.widgets import TextWidget as Text
+    from IPython.html.widgets import TextareaWidget as Textarea
+    from IPython.html.widgets import DropdownWidget as Dropdown
+    from IPython.html.widgets import RadioButtonsWidget as RadioButtons
+    from IPython.html.widgets import PopupWidget as Popup
+    from IPython.html.widgets import LatexWidget as Latex
+    from IPython.html.widgets import FloatTextWidget as FloatText
+    from IPython.html.widgets import ImageWidget as Image
+    VBox = Box
+    HBox = Box
+else:
+    from ipywidgets import *
+    
+from IPython.display import clear_output, display
 
 import matplotlib.pyplot as plt
 
@@ -24,13 +46,13 @@ class EditSymbols(object):
         symbols = {i:controller.symbols[i] for i in controller.symbols if i in controller.ds.dependent_variables + controller.ds.independent_variables}
         symbols.update({i:i for i in controller.ds.dependent_variables + controller.ds.independent_variables if i not in symbols})
         self.symbols = symbols
-        symbols_widgets = [widgets.TextWidget(description=i, value=self.symbols[i]) for i in symbols]
-        wi = widgets.ContainerWidget(children=symbols_widgets)
-        button = widgets.ButtonWidget(value=False, description='Done')
+        symbols_widgets = [Text(description=i, value=self.symbols[i]) for i in symbols]
+        wi = VBox(children=symbols_widgets)
+        button = Button(value=False, description='Done')
         button.on_click(self.update_symbols)
         button.symbols = symbols_widgets
         button.wi = wi
-        edit_symbols = widgets.ContainerWidget(description='Edit Symbols', children=[wi, button])
+        edit_symbols = VBox(description='Edit Symbols', children=[wi, button])
         controller.update_child('Edit Symbols', edit_symbols)
         wi.visible = True
     
