2d557e8c34bbfbd017f7ee84c309d87254dfcd9b
System Administrator <jlomn@ucdavis.edu>
2015-09-08 15:16:43 -0700

    Updated tables widget for IPython 4.0.0

--- dspace/display/UI/tables_widget.py
+++ dspace/display/UI/tables_widget.py
@@ -3,9 +3,33 @@ import dspace.plotutils
 import dspace.display
 
 import numpy as np
-from IPython.html.widgets import interact, interactive, fixed
-from IPython.html import widgets
-from IPython.display import clear_output, display, HTML, Latex
+
+from distutils.version import LooseVersion, StrictVersion
+
+import IPython
+
+if StrictVersion(IPython.__version__) < StrictVersion('4.0.0'):
+    from IPython.html.widgets import interact, interactive, fixed
+    from IPython.html.widgets import HTMLWidget as HTML
+    from IPython.html.widgets import TabWidget as Tab
+    from IPython.html.widgets import CheckboxWidget as Checkbox
+    from IPython.html.widgets import ButtonWidget as Button
+    from IPython.html.widgets import ContainerWidget as Box
+    from IPython.html.widgets import TextWidget as Text
+    from IPython.html.widgets import TextareaWidget as Textarea
+    from IPython.html.widgets import DropdownWidget as Dropdown
+    from IPython.html.widgets import RadioButtonsWidget as RadioButtons
+    from IPython.html.widgets import PopupWidget as Popup
+    from IPython.html.widgets import LatexWidget as Latex
+    from IPython.html.widgets import FloatTextWidget as FloatText
+    from IPython.html.widgets import ImageWidget as Image
+    VBox = Box
+    HBox = Box
+else:
+    from ipywidgets import *
+    
+from IPython.display import clear_output, display
+
 
 class DisplayTables(object):
     
@@ -16,7 +40,7 @@ class DisplayTables(object):
     def create_tables_widget(self):
         
         controller = self.controller
-        self.tables_widget = widgets.ContainerWidget()
+        self.tables_widget = VBox()
         controller.update_child('Tables', self.tables_widget)
         
     def add_table(self, table_data):
@@ -29,8 +53,8 @@ class DisplayTables(object):
         self.add_table_widget(table_data)
         
     def add_table_widget(self, table_data):
-        html_widget = widgets.HTMLWidget(value=table_data)
-        wi = widgets.PopupWidget(children=[html_widget])
+        html_widget = HTML(value=table_data)
+        wi = Popup(children=[html_widget])
         wi.set_css('height', '300px')
         children = [i for i in self.tables_widget.children]      
         children.append(wi)
