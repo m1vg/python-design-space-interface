ce03b91d734e7264a14b6d83c843336a7d715b0d
Jason Lomnitz <jlomn@ucdavis.edu>
2015-09-08 14:45:05 -0700

    Updated case widget for IPython 4.0.0

--- dspace/display/UI/case_widget.py
+++ dspace/display/UI/case_widget.py
@@ -2,9 +2,29 @@ import dspace
 import dspace.plotutils
 import dspace.display
 
-from IPython.html.widgets import interact, interactive, fixed
-from IPython.html import widgets
-from IPython.display import clear_output, display, HTML, Latex
+from distutils.version import LooseVersion, StrictVersion
+
+import IPython
+
+if StrictVersion(IPython.__version__) < StrictVersion('4.0.0'):
+    from IPython.html.widgets import interact, interactive, fixed
+    from IPython.html.widgets import HTMLWidget as HTML
+    from IPython.html.widgets import TabWidget as Tab
+    from IPython.html.widgets import CheckboxWidget as Checkbox
+    from IPython.html.widgets import ButtonWidget as Button
+    from IPython.html.widgets import ContainerWidget as Box
+    from IPython.html.widgets import TextWidget as Text
+    from IPython.html.widgets import TextareaWidget as Textarea
+    from IPython.html.widgets import DropdownWidget as Dropdown
+    from IPython.html.widgets import RadioButtonsWidget as RadioButtons
+    from IPython.html.widgets import PopupWidget as Popup
+    from IPython.html.widgets import LatexWidget as Latex
+    VBox = Box
+    HBox = Box
+else:
+    from ipywidgets import *
+    
+from IPython.display import clear_output, display, Latex
 
 import matplotlib.pyplot as plt
 
@@ -22,23 +42,23 @@ class CaseReport(object):
         controller = self.controller 
         if controller.ds is None:
             return
-        by_signature = widgets.CheckboxWidget(
+        by_signature = Checkbox(
                         description='Cases indicated by signature?', 
                         value=self.by_signature
                         )
-        case_id = widgets.TextWidget(description='* Analysis for case:')        
+        case_id = Text(description='* Analysis for case:')        
         if 'biological_constraints' not in controller.options:
             bio_constraints = ''
         else:
             bio_constraints = ', '.join(controller.defaults('biological_constraints')) 
-        constraints = widgets.TextareaWidget(description='Biological constraints:',
+        constraints = Textarea(description='Biological constraints:',
                                              value=bio_constraints)
-        button = widgets.ButtonWidget(description='Create Analysis')
+        button = Button(description='Create Analysis')
         button.on_click(self.create_report)
         button.case_id = case_id
         button.by_signature = by_signature
         button.constraints = constraints
-        wi = widgets.ContainerWidget(children=[by_signature,
+        wi = VBox(children=[by_signature,
                                                case_id,
                                                constraints,
                                                button])
@@ -81,21 +101,21 @@ class DisplayCase(object):
         if controller.ds is None:
             return
         case = self.case
-        self.info = widgets.ContainerWidget()
-        self.equations = widgets.ContainerWidget()
-        self.log_gains = widgets.ContainerWidget()
-        self.parameter_table = widgets.ContainerWidget() 
-        self.tolerances_table = widgets.ContainerWidget() 
-        self.bounding_box_table = widgets.ContainerWidget()
-        calculate_pvals = widgets.ButtonWidget(description='Determine values for the parameters')
+        self.info = VBox()
+        self.equations = VBox()
+        self.log_gains = VBox()
+        self.parameter_table = VBox() 
+        self.tolerances_table = VBox() 
+        self.bounding_box_table = VBox()
+        calculate_pvals = Button(description='Determine values for the parameters')
         calculate_pvals.visible = False
         calculate_pvals.on_click(self.identify_parameters)
         if case.is_valid() is True:
             if self.pvals is None:
                 calculate_pvals.visible = True
-        close_button = widgets.ButtonWidget(description='Close Tab')
+        close_button = Button(description='Close Tab')
         close_button.on_click(self.close_widget)
-        wi = widgets.PopupWidget(children=[self.info, 
+        wi = Popup(children=[self.info, 
                                            self.equations,
                                            self.log_gains,
                                            self.bounding_box_table,
@@ -129,13 +149,13 @@ class DisplayCase(object):
         
     def update_info(self):
         controller = self.controller 
-        html = widgets.HTMLWidget()
+        html = HTML()
         html_string = '<b>Name:</b> ' + controller.name + '<br>'
         html_string += '<b>Case Number:</b> ' + self.case.case_number + '<br>'
         html_string += '<b>Case Signature:</b> (' + self.case.signature + ')<br>'
         html_string += '<b>Valid:</b> ' + str(self.case.is_valid()) + '<br>'
         html_string += '<b>Is Cyclical:</b> ' + str(self.case.is_cyclical) + '<br><hr>'
-        check_box = widgets.CheckboxWidget(description='Logarithmic coordinates', 
+        check_box = Checkbox(description='Logarithmic coordinates', 
                                            value=self.log_coordinates)
         check_box.on_trait_change(self.change_logarithmic, 'value')
         html.value = html_string
@@ -144,20 +164,20 @@ class DisplayCase(object):
     def update_equations(self):
         controller = self.controller 
         case = self.case
-        check_box = widgets.CheckboxWidget(description='Show only dynamical system?', 
+        check_box = Checkbox(description='Show only dynamical system?', 
                                            value=self.dynamic_only)
         check_box.on_trait_change(self.change_dynamic_only, 'value')
         if self.case.ssystem.solution is None:
             check_box.disables = True            
-        equations = widgets.LatexWidget()
+        equations = Latex()
         if self.dynamic_only is False:
             equations.value = case.equations._repr_latex_()
         else:
             ssys = case.ssystem.remove_algebraic_constraints()
             equations.value = ssys.equations._repr_latex_()
-        solution = widgets.LatexWidget()
-        conditions = widgets.LatexWidget() 
-        boundaries = widgets.LatexWidget() 
+        solution = Latex()
+        conditions = Latex() 
+        boundaries = Latex() 
         if self.log_coordinates is True:
             conditions.value = case.conditions_log._repr_latex_()
             solution_data = case.ssystem.solution_log
@@ -173,13 +193,13 @@ class DisplayCase(object):
             solution.value = 'S-System has no solution.'
             boundaries.value = 'S-System has no solution.'
         self.equations.children = [check_box,
-                                   widgets.HTMLWidget(value='<b>Equations:</b><br>'),
+                                   HTML(value='<b>Equations:</b><br>'),
                                    equations, 
-                                   widgets.HTMLWidget(value='<b>Conditions:</b><br>'),
+                                   HTML(value='<b>Conditions:</b><br>'),
                                    conditions, 
-                                   widgets.HTMLWidget(value='<b>Solution:</b><br>'),
+                                   HTML(value='<b>Solution:</b><br>'),
                                    solution,
-                                   widgets.HTMLWidget(value='<b>Boundaries:</b><br>'),
+                                   HTML(value='<b>Boundaries:</b><br>'),
                                    boundaries]
 
     def update_log_gains(self):
@@ -188,7 +208,7 @@ class DisplayCase(object):
         if case.ssystem.solution is None:
             self.log_gains.children = []
             return
-        table = widgets.HTMLWidget()
+        table = HTML()
         html_str = '<div><table>\n<caption>Logarithmic gains and parameter sensitivities for Case ' + case.case_number + ' (' + case.signature + '). </caption>\n'
         html_str += '<tr ><th rowspan="2" align=center  style="padding:0 15px 0 15px;"> Dependent<br> Variables </th>'
         html_str += '<th colspan="' + str(len(case.independent_variables)) + '" align=center  style="padding:0 15px 0 15px;"> Independent Variables and Parameters</th></tr><tr align=center>'
@@ -201,11 +221,11 @@ class DisplayCase(object):
                 html_str += '<td align=center  style="padding:0 15px 0 15px;">{0}</td>'.format(str(case.ssystem.log_gain(xd, xi)))
             html_str += '</tr>\n'
         html_str += '</table></div><br>'
-        save_button = widgets.ButtonWidget(description='Save Log-Gain Table')
+        save_button = Button(description='Save Log-Gain Table')
         save_button.table_data = html_str
         save_button.on_click(self.save_table)
         table.value = html_str
-        self.log_gains.children = [widgets.HTMLWidget(value='<br>'),
+        self.log_gains.children = [HTML(value='<br>'),
                                    save_button, 
                                    table]
         return
@@ -217,7 +237,7 @@ class DisplayCase(object):
         if case.is_valid(p_bounds=pvals) is False:
             self.bounding_box_table.children = []
             return            
-        table = widgets.HTMLWidget()
+        table = HTML()
         html_str = '<div><table>\n<caption>Bounding box determined for Case ' + case.case_number + ' (' + case.signature + '){0}. </caption>\n'.format(' in log-coordinates' if self.log_coordinates is True else '') 
         html_str += '<tr ><th align=center  rowspan=2 style="padding:0 15px 0 15px;"> Parameters </th><th colspan=2> Tolerance </th></tr>'
         html_str += '<tr><td style="padding:0 15px 0 15px;"><b> Lower bound</b></td><td style="padding:0 15px 0 15px;"><b> Upper bound</b></td></tr>'
@@ -232,10 +252,10 @@ class DisplayCase(object):
                          upper if upper < upper_th else '&infin;')
         html_str += '</table></div>'
         table.value = html_str
-        save_button = widgets.ButtonWidget(description='Save Bounding Box Table')
+        save_button = Button(description='Save Bounding Box Table')
         save_button.table_data = html_str
         save_button.on_click(self.save_table)
-        self.bounding_box_table.children = [widgets.HTMLWidget(value='<br>'),
+        self.bounding_box_table.children = [HTML(value='<br>'),
                                             save_button,
                                             table]
         return
@@ -250,9 +270,9 @@ class DisplayCase(object):
         if case.is_valid(p_bounds=pvals) is False:
             self.parameter_table.children = []
             return
-        make_nominal = widgets.ButtonWidget(description='Make Nominal Parameter Set')
+        make_nominal = Button(description='Make Nominal Parameter Set')
         make_nominal.on_click(self.save_parameters)
-        table = widgets.HTMLWidget()
+        table = HTML()
         html_str = '<div><table>\n<caption>Value for the parameters automatically determined for Case ' + case.case_number + ' (' + case.signature + '). </caption>\n'
         html_str += '<tr ><th align=center  style="padding:0 15px 0 15px;"> Parameters </th><th> Value </th>'
         for xi in sorted(pvals.keys()):
@@ -261,10 +281,10 @@ class DisplayCase(object):
                              pvals[xi])
         html_str += '</table></div>'
         table.value = html_str
-        save_button = widgets.ButtonWidget(description='Save Parameter Table')
+        save_button = Button(description='Save Parameter Table')
         save_button.table_data = html_str
         save_button.on_click(self.save_table)
-        self.parameter_table.children = [widgets.HTMLWidget(value='<br>'),
+        self.parameter_table.children = [HTML(value='<br>'),
                                          save_button,
                                          table,
                                          make_nominal
@@ -281,7 +301,7 @@ class DisplayCase(object):
         if case.is_valid(p_bounds=pvals) is False:
             self.tolerances_table.children = []
             return            
-        table = widgets.HTMLWidget()
+        table = HTML()
         html_str = '<div><table>\n<caption>Global tolerances determined for Case ' + case.case_number + ' (' + case.signature + ') showing fold-difference to a large qualitative change{0}. </caption>\n'.format(' in log-coordinates' if self.log_coordinates is True else '') 
         html_str += '<tr ><th align=center  rowspan=2 style="padding:0 15px 0 15px;"> Parameters </th><th colspan=2> Tolerance </th></tr>'
         html_str += '<tr><td style="padding:0 15px 0 15px;"><b> Lower bound</b></td><td style="padding:0 15px 0 15px;"><b> Upper bound</b></td></tr>'
@@ -299,10 +319,10 @@ class DisplayCase(object):
         html_str += '; '.join([i + ' = ' + str(pvals[i]) for i in sorted(pvals.keys())]) + '.'
         html_str += '</caption></div>'
         table.value = html_str
-        save_button = widgets.ButtonWidget(description='Save Global Tolerance Table')
+        save_button = Button(description='Save Global Tolerance Table')
         save_button.table_data = html_str
         save_button.on_click(self.save_table)
-        self.tolerances_table.children = [widgets.HTMLWidget(value='<br>'),
+        self.tolerances_table.children = [HTML(value='<br>'),
                                           save_button,
                                           table]
         return
